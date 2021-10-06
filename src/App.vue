3fec0ee9e4682b896263b3dcccdfb8cc9298f803
Luis Igreja <lfsigreja@gmail.com>
2021-10-06 18:26:52 -0300

    props

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <p v-for="todo in doneTodos" :key="todo.text">
+      {{ todo.text }}
+    </p>
+
+    <button @click="checkAllTodos">Finalizar</button>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+
+interface Todo {
+  text: string
+  done: boolean
+}
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      todos: [] as Todo[],
+    };
+  },
+  computed: {
+    doneTodos(): Todo[] {
+      return this.todos.filter((todo) => todo.done);
+    },
+  },
+  watch: {
+    todos(newValue: Todo[]) {
+      const isFinished = !newValue.some(({ done }) => !done);
+
+      if (isFinished) {
+        alert('Heya');
+      }
+    },
+  },
+  created() {
+    this.todos = [
+      { text: 'Estudar Typescript', done: true },
+      { text: 'Lavar os pratos', done: false },
+      { text: 'Aprender Nuxt.js', done: true },
+    ];
+  },
+  methods: {
+    checkAllTodos() {
+      this.todos = this.todos.map(({ text }) => ({ text, done: true }));
+    },
   },
 });
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
